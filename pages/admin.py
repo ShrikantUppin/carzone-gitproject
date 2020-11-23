f182d1a51866aebf7f9b289e26dc239afbad50a9
ShrikantUppin <shrikantuppin@gmail.com>
2020-11-23 19:13:39 +0530

    fourth topic

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+
+
+# admin page customization
+
+class TeamAdmin(admin.ModelAdmin):
+
+    def thumbnil(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px"/>'.format(object.photo.url))
+
+    thumbnil.short_description = 'photo'
+    list_display = ('id', 'thumbnil', 'first_name', 'designation','created_date')
+    list_display_links = ('id','thumbnil','first_name',)
+    search_fields = ('id', 'first_name', 'last_name', 'designation')
+    list_filter = ('designation',)
+
+
+admin.site.register(Team, TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# model no. 1: Creating model for Team
+
+class Team(models.Model):
+    ''' to build team model we required following details /
+    firstname, lastname, photo,  designation, facebook_link, twitter_link, google_plus'''
+
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
